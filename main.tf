ab3d4901623965d85f2140f230ac640fd1b6875c
ChetanatmaP <148168494+ChetanatmaP@users.noreply.github.com>
2024-09-16 17:33:46 +0530

    update many sily mistake

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
+data "aws_vpc" "default" {
   default = true
 }
 
@@ -30,38 +30,38 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name        = "blog"
   description = "Allow http and https in. Allow everything out."
 
   vpc_id = "data.aws_vpc.default.id"
 }
 
-resource "aws_security_group_rule" "blog_http" {
+resource "aws_security_group_rule" "blog_http_in" {
   type         = "ingress"
-  from_port    = "80"
-  to_port      = "80"
+  from_port    = 80
+  to_port      = 80
   protocol     = "tcp"
   cidr_blocks  = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https" {
   type         = "ingress"
-  from_port    = "443"
-  to_port      = "443"
+  from_port    = 443
+  to_port      = 443
   protocol     = "tcp"
   cidr_blocks  = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_outbond" {
   type         = "egress"
-  from_port    = "0"
-  to_port      = "0"
+  from_port    = 0
+  to_port      = 0
   protocol     = "-1"
   cidr_blocks  = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
 }
