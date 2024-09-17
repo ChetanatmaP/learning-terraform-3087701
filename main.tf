ae962b125a5b1803ab0ff7aee0bd3f2969e332e8
ChetanatmaP <148168494+ChetanatmaP@users.noreply.github.com>
2024-09-17 17:12:20 +0530

    doit

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arn = aws_lb_target_group.blog_alb.arn
+  autoscaling_group_target_group_arns = module.blog_alb.arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
