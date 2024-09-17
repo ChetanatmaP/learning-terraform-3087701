6e4a073ea608366eded586a06faf6ad75b9e1c70
ChetanatmaP <148168494+ChetanatmaP@users.noreply.github.com>
2024-09-17 15:53:35 +0530

    do it

--- main.tf
+++ main.tf
@@ -54,11 +54,23 @@ module "alb" {
 
 
   listeners = {
+    ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-    
-    forward = {
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
         target_group_key = "ex-instance"
+      }
     }
   }
 
