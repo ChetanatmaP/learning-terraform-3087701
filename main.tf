d59efdfe876ea004de698d7dde4ac2a55d48f982
ChetanatmaP <148168494+ChetanatmaP@users.noreply.github.com>
2024-09-17 17:00:24 +0530

    instance change

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  autoscaling_group_target_group_arns = module.blog_alb.autoscaling_group_target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
@@ -75,7 +76,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.web.id
+      target_id        = aws_instance.blog.id
     }
   }
 
