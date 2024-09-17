e8bbe9a05f3c05d658db94c08908cd0037643e2f
ChetanatmaP <148168494+ChetanatmaP@users.noreply.github.com>
2024-09-17 14:46:14 +0530

    alb configure

--- main.tf
+++ main.tf
@@ -44,6 +44,42 @@ resource "aws_instance" "web" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog_alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_group_id = [module.blog_sg.security_group_id]
+
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+    }
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
